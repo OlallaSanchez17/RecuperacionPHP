15ed2ac3ca12d7ee6873fcf4cb144d6039d4d78a
Deivd730 <davidcastill2005@gmail.com>
2025-03-31 21:37:39 +0200

    Se añadio nuevo codigo al registro.php

--- view/validar_login.php
+++ view/validar_login.php
@@ -5,9 +5,6 @@ session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $usuario = $_POST['usuario'];
     $clave = ($_POST['clave']);
-
-
-
     if ($usuario == "admin" && $clave == ('1234')) {
         $_SESSION['usuario'] = $usuario;
         header("Location: bienvenido.php");
